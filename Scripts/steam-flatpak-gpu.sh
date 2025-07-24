b75c7875613b09eb5036f33137f58a0ef815c3fc
meeksj84 <meeksj84@gmail.com>
2025-07-24 11:21:41 -0700

    updates

--- Scripts/steam-flatpak-gpu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-flatpak run \
-  --env=__NV_PRIME_RENDER_OFFLOAD=1 \
-  --env=__GLX_VENDOR_LIBRARY_NAME=nvidia \
-  --env=__VK_LAYER_NV_optimus=NVIDIA_only \
-  com.valvesoftware.Steam
